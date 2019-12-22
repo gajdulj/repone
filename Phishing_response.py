c60d7f88a4179a54540cfd1099ee5a3a774c808e
Jakub Gajdul <gajdulj@Jakubs-MacBook-Pro.local>
2019-12-22 20:44:15 +0100

    Improved logging

--- Phishing_response.py
+++ Phishing_response.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-This program sends randomly generated data to a fake goverment website.
+This program sends randomly generated data to a fake goverment website (phishing scammers).
 
 SMS:
 HMRC Have determined that you are eligible for a tax rebate of £228.88. 
@@ -26,14 +26,14 @@ with open('surnames.csv', 'r') as fileb:
   readerb = csv.reader(fileb)
   surnames = list(readerb)
 
-for entry in range (100):
+for number in range (10000):
     ccname = str(''.join(random.choice(firstnames)+[' ']+random.choice(surnames)).lower())
     ccno = rand_x_digit_num(16)
     ccexp =  str('0')+(str(random.randint(1,9)))+' '+'/'+ ' ' + str(random.randint(20,22))
     secode = random.randint(150,750)
     account = rand_x_digit_num(8)
     sortcode = str('0')+str(random.randint(3,8))+'-'+str(random.randint(3,8))+ str('0')+'-'+str(random.randint(11,55))
-    print ('Inserted: ' +ccname,ccno,ccexp,secode,account,sortcode)
+    print ('Insert {}: '.format(number+1)+ccname,ccno,ccexp,secode,account,sortcode)
     requests.post(url,allow_redirects = False, data ={
         'tes': r'csrfToken,user_id,password,profile:6180403181099740681::Z7PJmvIbwqpEkYXuwErxkhl6/47IiqWuUIG089Fk/n9Rs34jIFTbTGzC3cNDsii8GVso7bwbKSw3KI/uEjS7Ag==',
         'csrfToken': r'60e95235e20c5eed27a06f25a9f99e782833a652\\-1553630837725-185ce4b4a7e1ecf38c2b8b1a',
