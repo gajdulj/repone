3db6597f0a717d5695a4db7d4e02a545ca0be5bc
Jakub Gajdul <gajdulj@Jakubs-MacBook-Pro.local>
2019-12-23 17:30:03 +0100

    Added names list length

--- Phishing_response.py
+++ Phishing_response.py
@@ -15,24 +15,29 @@ import csv
 
 url= 'https://gov.uk-auth-c58.com/account/Finish.php?ssl_id=GfAlwBJ3gtIq17F6jurP3d9sHYPoGJCnYcIvOrz4VhvWpa3IEuyIIIapG0OnJqKICsdqUNvP40MubPcQkLy3uIsbJgysGjajMoJGbfwgfiKq8XhsJPvdyYohfXJWgUf2jZ'
 
+# Generate n digit number
 def rand_x_digit_num(x):    
-    return random.randint(10**(x-1), 10**x-1)  
-      
+    return random.randint(10**(x-1), (10**x)-1)  
+
+#Load 2 .csv with forenames and surnames
 with open('firstnames.csv', 'r') as filea:
   readera = csv.reader(filea)
   firstnames = list(readera)
+  print ('Loaded list of {} first names.'.format(len(firstnames)))
 
 with open('surnames.csv', 'r') as fileb:
   readerb = csv.reader(fileb)
   surnames = list(readerb)
+  print ('Loaded list of {} surnames.'.format(len(surnames)))
 
-for number in range (10000):
+
+for number in range (1000):
     ccname = str(''.join(random.choice(firstnames)+[' ']+random.choice(surnames)).lower())
     ccno = rand_x_digit_num(16)
     ccexp =  str('0')+(str(random.randint(1,9)))+' '+'/'+ ' ' + str(random.randint(20,22))
     secode = random.randint(150,750)
     account = rand_x_digit_num(8)
-    sortcode = str('0')+str(random.randint(3,8))+'-'+str(random.randint(3,8))+ str('0')+'-'+str(random.randint(11,55))
+    sortcode = str('0')+str(random.randint(1,9))+'-'+str(random.randint(1,9))+ str('0')+'-'+str(random.randint(11,55))
     print ('Insert {}: '.format(number+1)+ccname,ccno,ccexp,secode,account,sortcode)
     requests.post(url,allow_redirects = False, data ={
         'tes': r'csrfToken,user_id,password,profile:6180403181099740681::Z7PJmvIbwqpEkYXuwErxkhl6/47IiqWuUIG089Fk/n9Rs34jIFTbTGzC3cNDsii8GVso7bwbKSw3KI/uEjS7Ag==',
@@ -44,3 +49,4 @@ for number in range (10000):
         'account': account,
         'sortcode': sortcode
     })
+    
