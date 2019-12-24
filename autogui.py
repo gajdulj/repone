4aefe587a8d22a1ba5444af3b7aeea18c8d041f7
Jakub Gajdul <gajdulj@Jakubs-MacBook-Pro.local>
2019-12-24 13:43:58 +0100

    renamed the file

---
+++ autogui.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import time
+import pyautogui
+
+#Use this to spam people
+def spam_messages():
+    time.sleep(3)
+    for i in range (10):
+        pyautogui.typewrite('Hello world count:{} !'.format(i))  
+        pyautogui.press('enter') 
+
+#Use this to determine the location of coursor to use for other actions
+def displaypos():
+    while True:
+        time.sleep(1)
+        pointing_at = pyautogui.position()
+        print (pointing_at)
+
+#Use this to keep the mouse active
+def movearound():
+    time.sleep(1)
+    while True:
+        pyautogui.moveTo(x=100, y=80, duration=0.5)
+        time.sleep(3)
+        pyautogui.moveTo(x=900, y=80, duration=0.5)
+        time.sleep(3)
+    
+
+#displaypos()
+movearound()
+#spam_messages()
